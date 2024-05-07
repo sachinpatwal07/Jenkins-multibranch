7b6e1b11da364ec0bbfaec5f736785cb4717b912
sachinpatwal07 <sachinpatwal@webmobsoft.com>
2024-05-07 21:24:12 +0530

    revert: skip

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ app.get("/", (req, res) => {
   res.json("Hi from the Server develop server....");
 });
 
-console.log("hello ")
-console.log("world")
 app.listen(3000, () => {
   console.log("Server listening on port 3000.....");
 });
