24911024c39f6b06ed720ff284193b74bb7b80aa
sachinpatwal07 <sachinpatwal@webmobsoft.com>
2024-05-07 21:20:24 +0530

    feat: new featur

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.json("Hi from the Server develop server....");
 });
 
-
+console.log("hello ")
 app.listen(3000, () => {
   console.log("Server listening on port 3000.....");
 });
